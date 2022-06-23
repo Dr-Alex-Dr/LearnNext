7f02c8544f4f42cb09b3aef3d1de024a99e9916e
Dr-Alex-Dr <mr.lopin900@gmail.com>
2022-06-23 10:11:17 +0300

    routes

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+const Error = () => {
+    return (
+    <>
+        <h2>404</h2>
+        <p>Bro fucked you</p>
+        </>
+    )
+}
+
+export default Error;
---
+++ pages/posts.js
@@ -0,0 +1,10 @@
+const Posts = () => {
+    return (
+    <>
+        <h1>Posts list:</h1>
+        <p>Lorem ipsum follor sit emet consr</p>
+    </>
+    )
+  }
+  
+  export default Posts;
